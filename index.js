c65e5034fab3fa5781ce05751533a3ed3decc9e3
DouglasSochC <sochdouglas1623@gmail.com>
2023-02-14 13:31:36 -0600

    Correction of the 'add' requeriment; Now is multiplication

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(express.json());
 
 let resultado = 0;
-// Sumatoria entre dos valores
-app.post('/sumatoria', (req, res) => {
+// Multiplicacion entre dos valores
+app.post('/multiplicacion', (req, res) => {
   const { valor1, valor2 } = req.body;
-  resultado = valor1 + valor2;
+  resultado = valor1 * valor2;
   res.send({"mensaje":"La operacion se ha realizado correctamente"});
 });
 
