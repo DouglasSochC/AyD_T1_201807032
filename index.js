1927bfb387fbdac26111371497a9d12b4e19f125
DouglasSochC <sochdouglas1623@gmail.com>
2023-02-14 12:18:22 -0600

    Endpoint POST that add two values

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 // Se utilizara JSON en los request
 app.use(express.json());
 
+let resultado = 0;
+// Sumatoria entre dos valores
+app.post('/sumatoria', (req, res) => {
+  const { valor1, valor2 } = req.body;
+  resultado = valor1 + valor2;
+  res.send({"mensaje":"La operacion se ha realizado correctamente"});
+});
+
 // Agregando listener del servidor
 const port = process.env.port || 3000;
 app.listen(port, () => {
